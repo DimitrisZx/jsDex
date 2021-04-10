a1b2b6d56ab2cd30254b5e0cfe62f8324abab8c5
dimitris <dimitriszarahanis@gmail.com>
2021-04-11 01:26:30 +0300

    implement store

---
+++ src/store/store.ts
@@ -0,0 +1,57 @@
+import { Component } from "../helpers/component";
+import { PokedexService } from "../services/pokedex.service";
+
+export const store = {
+  state: {
+    currentPokemon: {}
+  },
+};
+
+
+export class Store {
+  private state: {[key: string]: any};
+  private subscribers: Component[] = [];
+  constructor(
+    initState = {},
+    private pokedexService = new PokedexService()) {
+    this.state = {...initState, currentPokemon: {}};
+  }
+
+  public async makeAsyncCall(endpoint: string, value: string): Promise<void> {
+    let callResult: any;
+    switch (endpoint) {
+      case 'getPokemonByName':
+        callResult = await this.pokedexService.getPokemonByName(value)
+        this.mutateState('currentPokemon', callResult)
+        console.log(this.state);
+        break;
+      default:
+        console.warn('No state was changed!')
+        break;
+    }
+  }
+
+  private mutateState(key: string, value: string): void {
+    if (key && value) {
+      this.state[key] = value;
+    }
+    this.informSubscribers();
+  }
+
+  public setState(payload: Payload): void {
+    this.mutateState(Object.keys(payload.data)[0], payload.data);
+  }
+
+  public getStateValue(itemKey: string):any {
+    return this.state[itemKey];
+  }
+
+  private informSubscribers():void {
+    this.subscribers.forEach((subscriber: any) => subscriber.inform({...this.state}))
+  }
+
+  public subscribe(subscriber: Component): string {
+    this.subscribers.push(subscriber)
+    return 'tesst';
+  }
+}
